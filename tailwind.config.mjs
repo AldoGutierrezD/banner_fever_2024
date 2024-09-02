f42483b255e90a0d651d010dd300ecad76acaae2
AldoGutierrezD <aldogd24@gmail.com>
2024-09-02 16:36:38 -0600

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'impact': ['Impact'],
+				'montserrat': ['Montserrat']
+			},
+			colors: {
+				'primary': '#fa6c06',
+				'secondary': '#8d78ff',
+				'accent': '#ffda74',
+				'complementary': '#ffdfc6',
+				'background': '#ededed',
+				'light': '#ffffff',
+				'dark': '#000000'
+			}
+		},
+	},
+	plugins: [],
+}
